5e512d88645221c74f1167ae16b3c5acdd0f5e72
RosieDuffy <rose.j.duffy@gmail.com>
2023-11-03 10:30:09 +0000

    Components initialized

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import apiKey from "./config";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
